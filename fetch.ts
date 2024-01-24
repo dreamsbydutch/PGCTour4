b7fb398f1c0673c4033a185fa31fa83b2a3e5a67
Curtis Hough <chough14@gmail.com>
2024-01-24 01:40:55 -0500

    add types to fetch

--- fetch.ts
+++ fetch.ts
@@ -1,6 +1,8 @@
-function fetch(endpoint,options) {
-  const optionString = Object.keys(options)
-  const json = UrlFetchApp.fetch('https://feeds.datagolf.com/'+endpoint+'?key=d3bc85650eb3306f6d6e2c0c4bd5', {'muteHttpExceptions': true}).getContentText()
+type dataGolfEndpoints = '' | 'preds/pre-tournament'
+
+function fetch(endpoint:dataGolfEndpoints,options:{}) {
+  const optionString = ''+Object.keys(options).map((item:string) => item+'='+options[item])
+  const json = UrlFetchApp.fetch('https://feeds.datagolf.com/'+endpoint+'?'+optionString+'key=d3bc85650eb3306f6d6e2c0c4bd5', {'muteHttpExceptions': true}).getContentText()
   const data = JSON.parse(json);
   console.log(data)
 }
